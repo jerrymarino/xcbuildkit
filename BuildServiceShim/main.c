8609a40458fe42095c429095c3f0b2cc26cf6138
Oscar Bonilla <6f6231@gmail.com>
2021-05-21 17:45:36 -0700

    Fix a small compilation bug (#26)
    ```
    BuildServiceShim/main.c:3:5: error: implicit declaration of function 'system' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
        system("/bin/bash -c \"$(dirname $XCBBUILDSERVICE_PATH)/BuildServiceShim.runfiles/__main__/BuildServiceShim/stub.sh\"");
        ^
    1 error generated.
    ```

--- BuildServiceShim/main.c
+++ BuildServiceShim/main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int main(int ac, char** av) {
     // Run a bash script "stub" adjacent to the binary
     system("/bin/bash -c \"$(dirname $XCBBUILDSERVICE_PATH)/BuildServiceShim.runfiles/__main__/BuildServiceShim/stub.sh\"");
