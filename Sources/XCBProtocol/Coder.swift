bba72ef7ce1de102f8faa4740301a5dced431279
Thiago Cruz <thiago@squareup.com>
2022-08-08 15:04:34 -0400

    undo custom log file path

--- Sources/XCBProtocol/Coder.swift
+++ Sources/XCBProtocol/Coder.swift
@@ -93,8 +93,8 @@ extension XCBDecoder {
     }
 }
 
-public func log(_ str: String, logPath: String = "/tmp/xcbuild.log") {
-    let url = URL(fileURLWithPath: logPath)
+public func log(_ str: String) {
+    let url = URL(fileURLWithPath: "/tmp/xcbuild.log")
     let entry = str + "\n"
     do {
         let fileUpdater = try FileHandle(forWritingTo: url)
