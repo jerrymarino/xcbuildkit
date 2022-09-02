9aab2a0e5ae326b24fbc2892339b74f31cd2cf60
Thiago Cruz <thiago@squareup.com>
2022-09-02 16:38:17 -0400

    rename some

--- Sources/BKBuildService/Service.swift
+++ Sources/BKBuildService/Service.swift
@@ -52,12 +52,12 @@ public class BKBuildService {
     // crossing streams.
     internal static let writeQueue = DispatchQueue(label: "com.xcbuildkit.bkbuildservice")
 
-    private var buffer2 = Data()
-    private var bufferHeader1 = Data()
-    private var bufferHeader2 = Data()
-    private var buffer2Next = Data()
-    private var readLen2: Int32 = 0
-    private var gotMsgId: UInt64 = 0
+    private var buffer = Data()
+    private var bufferMsgId = Data()
+    private var bufferContentSize = Data()
+    private var bufferNext = Data()
+    private var readLen: Int32 = 0
+    private var msgId: UInt64 = 0
     private var workingDir: String?
 
     // This is highly experimental
@@ -74,8 +74,8 @@ public class BKBuildService {
     }
 
     func handleIdx(messageHandler: @escaping XCBMessageHandler, context: Any?) {
-        let nowayResult = Unpacker.unpackAll(self.buffer2)
-        let nowayInput = XCBInputStream(result: nowayResult, data: self.buffer2, workingDir: self.workingDir)
+        let nowayResult = Unpacker.unpackAll(self.buffer)
+        let nowayInput = XCBInputStream(result: nowayResult, data: self.buffer, workingDir: self.workingDir)
         let nowayDecoder = XCBDecoder(input: nowayInput)
         let nowayMsg = nowayDecoder.decodeMessage()
 
@@ -83,43 +83,43 @@ public class BKBuildService {
             write([
                 XCBRawValue.string("PING"),
                 XCBRawValue.nil,
-            ], msgId: self.gotMsgId)
-            messageHandler(nowayInput, self.buffer2, context)
+            ], msgId: self.msgId)
+            messageHandler(nowayInput, self.buffer, context)
         } else {
             var ogData = Data()
-            ogData.append(self.bufferHeader1)
-            ogData.append(self.bufferHeader2)
-            ogData.append(self.buffer2)
+            ogData.append(self.bufferMsgId)
+            ogData.append(self.bufferContentSize)
+            ogData.append(self.buffer)
 
             var fooResult = [MessagePackValue]()
             var fooData = ogData
             if nowayMsg is CreateSessionRequest {
                 fooResult = nowayResult
-                fooData = self.buffer2
+                fooData = self.buffer
             }
 
             messageHandler(XCBInputStream(result: fooResult, data: fooData), ogData, context)
         }
 
-        self.buffer2 = Data()
-        self.bufferHeader1 = Data()
-        self.bufferHeader2 = Data()
-        self.readLen2 = 0
-        self.gotMsgId = 0
+        self.buffer = Data()
+        self.bufferMsgId = Data()
+        self.bufferContentSize = Data()
+        self.readLen = 0
+        self.msgId = 0
     }
 
     func collectHeaderInfo(data: Data) -> (Int32, Data) {
         var tmpData = data
         let readSizeFirst2 = MemoryLayout<UInt64>.size
         let msgIdData2 = tmpData[0 ..< readSizeFirst2]
-        self.bufferHeader1 = msgIdData2
+        self.bufferMsgId = msgIdData2
         let msgId2 = msgIdData2.withUnsafeBytes { $0.load(as: UInt64.self) }
         tmpData = tmpData.advanced(by: readSizeFirst2)
-        self.gotMsgId = msgId2
+        self.msgId = msgId2
 
         let readSizeSecond2 = MemoryLayout<UInt32>.size
         let sizeD2 = tmpData[0 ..< readSizeSecond2]
-        self.bufferHeader2 = sizeD2
+        self.bufferContentSize = sizeD2
         let sizeB2 = sizeD2.withUnsafeBytes { $0.load(as: UInt32.self) }
         let size2 = Int32(sizeB2)
         tmpData = tmpData.advanced(by: readSizeSecond2)        
@@ -129,36 +129,36 @@ public class BKBuildService {
 
     func initializeBuffer(size: Int32, data: Data) -> Bool {
         if size <= Int32(data.count) {
-            self.buffer2 = data
-            self.readLen2 = 0
+            self.buffer = data
+            self.readLen = 0
             return true
         }
         else {
-            self.buffer2.append(data)
-            self.readLen2 = min(max(size - Int32(data.count), 0), Int32(data.count))
+            self.buffer.append(data)
+            self.readLen = min(max(size - Int32(data.count), 0), Int32(data.count))
             return false
         }        
     }
 
     func appendBuffer(data: Data) -> Bool {
-        if self.readLen2 > serializerToken {
-            self.buffer2.append(data)
-            self.readLen2 = self.readLen2 - Int32(data.count)
+        if self.readLen > serializerToken {
+            self.buffer.append(data)
+            self.readLen = self.readLen - Int32(data.count)
             return false
-        } else if self.readLen2 > 0 {
+        } else if self.readLen > 0 {
             var tmpData = data
 
-            var finalData = tmpData[0 ..< Int(self.readLen2)]
-            self.buffer2.append(finalData)
+            var finalData = tmpData[0 ..< Int(self.readLen)]
+            self.buffer.append(finalData)
 
-            tmpData = tmpData.advanced(by: Int(self.readLen2))
+            tmpData = tmpData.advanced(by: Int(self.readLen))
 
             if tmpData.count > 0 {
-                self.buffer2Next = tmpData
+                self.bufferNext = tmpData
             } else {
-                self.buffer2Next = Data()
+                self.bufferNext = Data()
             }
-            self.readLen2 = 0
+            self.readLen = 0
             return true
         }
 
@@ -178,7 +178,7 @@ public class BKBuildService {
             // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ START
             var readyToProcess = false
 
-            if self.buffer2.count == 0 {
+            if self.buffer.count == 0 {
                 var (size2, tmpData) = collectHeaderInfo(data: data)
                 readyToProcess = initializeBuffer(size: size2, data: tmpData)
             } else {
@@ -188,13 +188,13 @@ public class BKBuildService {
             if readyToProcess {
                 handleIdx(messageHandler: messageHandler, context: context)
 
-                if self.buffer2Next.count > 0 {
-                    var (size2, tmpData) = collectHeaderInfo(data: self.buffer2Next)
+                if self.bufferNext.count > 0 {
+                    var (size2, tmpData) = collectHeaderInfo(data: self.bufferNext)
                     readyToProcess = initializeBuffer(size: size2, data: tmpData)
 
                     if readyToProcess {
                         handleIdx(messageHandler: messageHandler, context: context)
-                        self.buffer2Next = Data()
+                        self.bufferNext = Data()
                     }
                 }
             }
